d017020cd7e6b6aad1df99435e19f8c5438303a8
Bluebird-PAP <126821543+pablopeinipil@users.noreply.github.com>
2023-03-30 22:39:30 -0300

    Update de TP
    Actualizacion de trabajo Practico al dia de la fecha

---
+++ Curso_ArgentinaPrograma-PabloPeinipil/src/Ronda.java
@@ -0,0 +1,30 @@
+public class Ronda {
+
+
+    private String NumRonda;
+    private int NumPartidos;
+
+
+
+    // GETTER Y SETTER
+
+
+    public String getNumRonda() {
+        return NumRonda;
+    }
+
+    public void setNumRonda(String numRonda) {
+        NumRonda = numRonda;
+    }
+
+    public int getNumPartidos() {
+        return NumPartidos;
+    }
+
+    public void setNumPartidos(int numPartidos) {
+        NumPartidos = numPartidos;
+    }
+
+
+
+}
---
+++ Curso_ArgentinaPrograma-PabloPeinipil/src/Servicios/IngresoDatos.java
@@ -0,0 +1,23 @@
+package Servicios;
+
+public class IngresoDatos {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Bienvenido a App de Resultados Mundial y Pronosticos");
+        System.out.println("-------------------------------");
+        System.out.println("Seleccione una opción:"+" (1)-Resultados Finales" + " (2)-Pronostico del Partido");
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
